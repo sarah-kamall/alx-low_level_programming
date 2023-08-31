5bfc5679eeb0109425ea6d439709ceabd1b5a311
Your Name <you@example.com>
2023-08-31 05:31:20 -0700

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,14 +1,14 @@
 #include"main.h"
-void print_binary(int x)
+void print_binary(unsigned long int n)
 {
 	unsigned long int mask;
 	int f;
 
 	f = 0;
-	mask = 1UL<< sizeof(x)*8-1;
+	mask = 1UL<< (sizeof(n)*8-1);
 	while (mask > 0)
 	{
-		if (x & mask)
+		if (n & mask)
 		{
 			_putchar('1');
 			f = 1;
