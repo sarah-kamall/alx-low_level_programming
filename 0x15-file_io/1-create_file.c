22325f4720cf9ee5b24961b36756579a855f7590
Your Name <you@example.com>
2023-09-07 00:39:31 +0300

    some adjusts

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,24 @@
 #include"main.h"
+/**
+* create_file - Create or overwrite a file with the given text content.
+* @filename: The name of the file to be created or overwritten.
+* @text_content: The content to be written to the file.
+*
+* Description:
+* This function creates a new file or overwrites an existing file with the
+* file doesn't exist, it creates a new empty file. The function then writes
+* the contents of the text_content string to the file. If the file already
+* exists, its previous contents are replaced with the new text content.
+*
+* Return:
+* On success, returns 1. On failure, returns -1.
+*
+* File Permissions:
+* The function uses the O_CREAT flag when opening the file, so it's important
+* to specify the desired file permissions (mode) when calling this function.
+* The recommended mode for most use cases is 0600, which grants read and write
+* permissions to the file owner and no permissions to others.
+*/
 int create_file(const char *filename, char *text_content)
 {
 	int ac, i, byteswritten, fp;
@@ -7,7 +27,7 @@ int create_file(const char *filename, char *text_content)
 	i = 0;
 	if (!filename)
 		return (-1);
-	ac = access(filename, W_OK);
+	ac = access(filename, W_OK | F_OK);
 	if (ac == -1)
 	{
 		fp = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,30 +1,39 @@
 #include"main.h"
+/**
+ * append_text_to_file - Appends text to a file or creates it if it doesn't exist.
+ * @filename: The name of the file.
+ * @text_content: The content to append.
+ *
+ * Description:
+ * Appends text_content to the specified file or creates a new file if it doesn't exist.
+ * Returns 1 on success, -1 on failure.
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
-    int fd, numofbytes, byteswritten, i;
-    char *content;
-    i = 0;
-
-    if (!filename)
-        return (-1);
-    if (access(filename, F_OK | W_OK) == -1)
-        return (-1);
-    else
-    {
-        fd = open(filename, O_RDWR | O_APPEND);
-        if (fd == -1)
-            return (-1);
-        content = text_content;
-        while (*text_content)
-        {
-            i++;
-            text_content++;
-        }
-        byteswritten = write(fd, content, i);
-        if (byteswritten == -1)
-            return (-1);
-        close(fd);
-    }
-    return (1);
+	int fd, numofbytes, byteswritten, i;
+	char *content;
+	
+	i = 0;
+	if (!filename)
+		return (-1);
+	if (access(filename, F_OK | W_OK) == -1)
+		return (-1);
+	else
+	{
+		fd = open(filename, O_RDWR | O_APPEND);
+		if (fd == -1)
+			return (-1);
+		content = text_content;
+		while (*text_content)
+		{
+			i++;
+			text_content++;
+		}
+		byteswritten = write(fd, content, i);
+		if (byteswritten == -1)
+			return (-1);
+		close(fd);
+	}
+	return (1);
 }
 
