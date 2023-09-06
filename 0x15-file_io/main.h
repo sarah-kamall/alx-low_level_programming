14eb6bcbf244f8728a121d6afc5c076145e2c08a
Your Name <you@example.com>
2023-09-06 22:21:07 +0300

    task 1

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stddef.h>
+#ifndef MAIN
+#define MAIN
+int create_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
