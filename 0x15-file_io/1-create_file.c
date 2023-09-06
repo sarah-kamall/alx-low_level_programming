43bcf39fe5e678e5a657637b7821d75696c8b088
Your Name <you@example.com>
2023-09-07 00:10:08 +0300

    betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,31 +1,32 @@
 #include"main.h"
 int create_file(const char *filename, char *text_content)
 {
-    int ac, i, byteswritten, fp;
-    char *context;
-    i = 0; 
-    if (!filename)
-        return (-1);
-    ac = access(filename, W_OK);
-    if (ac == -1)
-    {
-        fp = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-    }
-    else
-    {
-        fp = open(filename, O_RDWR);
-    }
-    if (fp == -1)
-        return (-1);
-    context = text_content;
-    while (*text_content)
-    {
-        i++;
-        text_content++;
-    }
-    byteswritten = write(fp, context, i);
-    if (byteswritten == -1)
-        return (-1);
-    return (1);
+	int ac, i, byteswritten, fp;
+
+	char *context;
+	i = 0;
+	if (!filename)
+		return (-1);
+	ac = access(filename, W_OK);
+	if (ac == -1)
+	{
+		fp = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	}
+	else
+	{
+		fp = open(filename, O_RDWR);
+	}
+	if (fp == -1)
+		return (-1);
+	context = text_content;
+	while (*text_content)
+	{
+		i++;
+		text_content++;
+	}
+	byteswritten = write(fp, context, i);
+	if (byteswritten == -1)
+		return (-1);
+	return (1);
 }
 
