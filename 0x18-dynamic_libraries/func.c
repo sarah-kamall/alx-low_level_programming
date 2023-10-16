d973b03cdc1caf250e8b589b446b0363b1b424dc
sarah-kamall <sarahkamal1712@gmail.com>
2023-10-16 23:00:33 +0300

    tsk 0

---
+++ 0x18-dynamic_libraries/func.c
@@ -0,0 +1,48 @@
+#include <stddef.h>
+int _putchar(char c)
+{
+	return c;}
+int _islower(int c)
+{
+	return c;}
+int _isalpha(int c){
+	return c;}
+int _abs(int n){
+	return n;}
+int _isupper(int c){
+	return c;}
+int _isdigit(int c){
+	return c;}
+int _strlen(char *s){
+	return 0;}
+void _puts(char *s){
+	return;}
+char *_strcpy(char *dest, char *src){
+	return NULL; }
+int _atoi(char *s){
+	return 0;}
+char *_strcat(char *dest, char *src){
+	return NULL;}
+char *_strncat(char *dest, char *src, int n){
+	return NULL;}
+char *_strncpy(char *dest, char *src, int n)
+{
+	return NULL;}
+int _strcmp(char *s1, char *s2){
+	return 0;}
+char *_memset(char *s, char b, unsigned int n)
+{
+	return s;}
+char *_memcpy(char *dest, char *src, unsigned int n)
+{	return NULL;}
+char *_strchr(char *s, char c)
+{	return NULL;}
+unsigned int _strspn(char *s, char *accept)
+{	return 0;}
+char *_strpbrk(char *s, char *accept)
+{
+	return NULL;}
+
+char *_strstr(char *haystack, char *needle)
+{
+	return NULL;}
