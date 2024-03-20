fbcebb88af7ee09a8bf0636b6fb999ce3332565a
sarah-kamall <sarahkamal1712@gmail.com>
2024-03-20 23:58:50 +0200

    maybe;

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,9 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include"school.h"
+int main()
+{
+    printf("hello\n");
+    mprint();
+    return 0;
+}
---
+++ 0x1C-makefiles/school.c
@@ -0,0 +1,8 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int mprint()
+{
+    printf("hello school");
+    return 0;
+}
